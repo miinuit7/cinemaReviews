8664a954a0381d101340bc865f942297de12e864
miinuit7 <silk_728@yahoo.co.jp>
2024-07-21 00:25:16 +0900

    コミットテスト

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,10 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>シネマレビューズ</title>
 
         <!-- Fonts -->
+        <link rel="icon" href="{{ asset('cinemaFavicon.ico') }}" type="image/x-icon">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
